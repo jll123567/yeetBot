2a40c1e076c085f32bb96b17f4e30c6fa55fa574
jl123567 <ledbetterte@gmail.com>
2018-09-27 21:35:47 -0400

    some small updates

--- mainNoToken.py
+++ mainNoToken.py
@@ -1,7 +1,7 @@
 # YEET
-import discord
-import asyncio
 from random import randint
+
+import discord
 
 client = discord.Client()
 
@@ -9,8 +9,11 @@ yeetList = ["ʏɛɛȶ",
             "yee\nwait...\nyeet*",
             "yeetn't",
             "its ya boi, **YEET**",
-            "https://images.prod.meredith.com/product/f6a4f6da9db798cd5a762b861149b4a6/1514196065413/l/womens-funny-eat-sleep-yeet-repeat-popular-dance-tshirt-urban-slang-large-royal-blue"
+            "https://images.prod.meredith.com/product/f6a4f6da9db798cd5a762b861149b4a6/1514196065413/l/womens-funny"
+            "-eat-sleep-yeet-repeat-popular-dance-tshirt-urban-slang-large-royal-blue",
+            "https://pre00.deviantart.net/338a/th/pre/f/2017/306/0/9/new_canvastdvybujk_by_mintivy-dbshzlu.png"
             ]
+
 
 @client.event
 async def on_ready():
@@ -22,15 +25,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('y!test'):
-        await client.send_message(message.channel, 'hello')
-
     if message.content.startswith('y!help'):
         await client.send_message(message.channel, "```y!yeet | yeet\n"
                                                    "m!help | this```")
 
     if message.content.startswith('y!yeet'):
-        await client.send_message(message.channel, yeetList[randint(0, yeetList.__len__())])
+        await client.send_message(message.channel, yeetList[randint(0, yeetList.__len__() - 1)])
+
 
 client.run('token')
 # https://discordapp.com/oauth2/authorize?client_id=494971719686291456&scope=bot&permissions=2048
